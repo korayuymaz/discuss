5f83de9e421050df9a0ac34ec7842a6427b92dd9
Koray <koray.uymaz@agesa.com.tr>
2024-02-22 19:22:01 +0300

    Header auth set inside a different component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,61 +1,14 @@
-import Link from "next/link";
 import {
+  Input,
   Navbar,
   NavbarBrand,
-  NavbarItem,
-  Input,
-  Button,
-  Avatar,
   NavbarContent,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+  NavbarItem,
 } from "@nextui-org/react";
-import { auth } from "@/auth";
+import Link from "next/link";
+import HeaderAuth from "./header-auth";
 
-import * as actions from "@/actions";
-
-export default async function Header(params: type) {
-  const session = await auth();
-
-  let authContent: React.ReactNode;
-
-  if (session?.user) {
-    authContent = (
-      <Popover placement="left">
-        <PopoverTrigger>
-          <Avatar src={session.user.image || ""} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className="p-4">
-            <form action={actions.signOut}>
-              <Button type="submit">Sign Out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    );
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="secondary" variant="bordered">
-              Sign In
-            </Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="primary" variant="flat">
-              Sign Up
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
-
+export default function Header(params: type) {
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
@@ -68,7 +21,9 @@ export default async function Header(params: type) {
           <Input />
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">{authContent}</NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
